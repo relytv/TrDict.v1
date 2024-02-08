226d24955771f624037f32c104b45b917ded0f9b
Gleb <shchebetko@gmail.com>
2024-02-08 13:14:47 +0500

    Add config.py

---
+++ settings/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_URL: str
+
+    class Config:
+        env_file = "settings/.env"
+
+
+settings = Settings()
+
+
