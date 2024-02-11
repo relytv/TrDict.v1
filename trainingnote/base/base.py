7b01fa93f98a927f9414bee877048f9c928883ed
Gleb <shchebetko@gmail.com>
2024-02-11 18:14:16 +0500

    Add update and delete

--- trainingnote/base/base.py
+++ trainingnote/base/base.py
@@ -1,5 +1,5 @@
 from trainingnote.database import async_session_maker
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, update
 
 
 class Base:
@@ -13,6 +13,28 @@ class Base:
             return res.scalar_one_or_none()
 
     @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+            res = await session.execute(query)
+            return res.scalars().all()
+
+    @classmethod
+    async def delete(cls, model_id: int):
+        async with async_session_maker() as session:
+            model = await session.get(cls.model, model_id)
+            await session.delete(model)
+            await session.commit()
+            return {"deleted": True}
+
+    @classmethod
     async def add(cls, **data):
         async with async_session_maker() as session:
             query = insert(cls.model).values(**data)
@@ -20,9 +42,8 @@ class Base:
             await session.commit()
 
     @classmethod
-    async def find_one_or_none(cls, **filter_by):
+    async def update(cls, model_id, **data):
         async with async_session_maker() as session:
-            query = select(cls.model).filter_by(**filter_by)
-            result = await session.execute(query)
-            return result.scalar_one_or_none()
-        
+            query = update(cls.model).values(**data).where(cls.model.id == model_id)
+            await session.execute(query)
+            await session.commit()
--- trainingnote/training/router.py
+++ trainingnote/training/router.py
@@ -2,6 +2,7 @@ from datetime import date
 from fastapi import APIRouter, Depends
 from trainingnote.training.dao import TrainingDAO
 from trainingnote.user.dependencies import get_current_user
+from trainingnote.user.models import User
 
 
 router = APIRouter(prefix="/trainigs", tags=["Trainings"])
@@ -11,6 +12,22 @@ router = APIRouter(prefix="/trainigs", tags=["Trainings"])
 async def add_training(
     description: str,
     date: date | None = None,
-    user=Depends(get_current_user),
+    user: User = Depends(get_current_user),
 ):
     return await TrainingDAO.add(user_id=user.id, description=description, date=date)
+
+
+@router.get("/get_all")
+async def get_all(user: User = Depends(get_current_user)):
+    return await TrainingDAO.find_all(user_id=user.id)
+
+
+@router.delete("/delete")
+async def delete_training(training_id: int, user: User = Depends(get_current_user)):
+    return await TrainingDAO.delete(model_id=training_id)
+
+
+@router.patch("/update/{training_id}")
+async def update_training(training_id: int, new_description: str | None = None, user: User = Depends(get_current_user)):
+    await TrainingDAO.update(training_id, description=new_description)
+    return {"updated_fields": {"description": new_description}}
